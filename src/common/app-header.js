61a336141763f723ce26daf582d7c5a26cc62ba7
izharkhanit <33596771+izharkhanit@users.noreply.github.com>
2020-07-27 02:02:44 +0200

    Update app-header.js

--- src/common/app-header.js
+++ src/common/app-header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Binance from './Binance.svg'
+//import Binance from './Binance.svg'
 
 const AppHeader = () => (
     <header>
